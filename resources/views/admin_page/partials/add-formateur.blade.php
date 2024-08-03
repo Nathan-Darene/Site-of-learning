65cf9a3abaa17684891f5cf36f01aca6a15198b9
Nathan-Darene <Nathandarene85@gmail.com>
2024-08-03 10:03:15 +0000

    Conception de la page user et admin

--- resources/views/admin_page/partials/add-formateur.blade.php
+++ resources/views/admin_page/partials/add-formateur.blade.php
@@ -4,22 +4,8 @@
         <span class="close">&times;</span>
         <center><h2 class="titre_">Ajouter un formateur</h2></center>
         <div id="Add-formateur-list">
-            <div class="Add-user-lists">
-                {{-- @foreach (\App\Models\Users::inRandomOrder() /*->take(6)*/->get() as $user) --}}
-                    <div class="user delete-user">
-                        {{-- @if ($user->imageUpload) --}}
-                            <img src="" class="image">
-                        {{-- @else --}}
-                            <img src="{{ asset('uploads/user.png') }}">
-                        {{-- @endif --}}
-                        <h2 class="h2">username</h2>
-                        <p class="color">city</p>
-                        <p class="color">gender</p>
-                        <p class="h3">created</p>
-                        <i class="fas fa-trash-can delete-icon" title="supprimer l'utilisateur"
-                            data-user-id="id"></i>
-                    </div>
-                {{-- @endforeach --}}
+            <div class="Add-formateur-lists">
+                
             </div>
         </div>
     </div>
